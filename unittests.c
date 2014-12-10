090fb125e9de3b105e87bc875667f4042800abb7
Hullberg <johan.hullberg@gmail.com>
2014-12-10 13:50:40 +0100

    Changed tests to consecutive codestandard

--- unittests.c
+++ unittests.c
@@ -28,9 +28,9 @@ int clean_suite_1(void)
 }
 
 /**
-@brief Tests the funcion equalProducts
+@brief Tests the funcion equal_products
  */
-void testLAGER_equalProducts(void)
+void testLAGER_equal_products(void)
 {
   struct product_t product1;
   product1.name = "Apple";
@@ -45,13 +45,13 @@ void testLAGER_equalProducts(void)
   product2.place = "B02";
   product2.price = 20;
   product2.amount = 7;
-  CU_ASSERT(equalProducts(product1, product1) == true);
-  CU_ASSERT(equalProducts(product1, product2) == false);
+  CU_ASSERT(equal_products(product1, product1) == true);
+  CU_ASSERT(equal_products(product1, product2) == false);
 }
 /**
 @brief Tests the function add_to_db
  */
-void testLAGER_addToDB(void) 
+void testLAGER_add_to_db(void) 
 {
   db_t db1 = malloc(sizeof(struct db_t));
   struct product_t product1;
@@ -71,13 +71,13 @@ void testLAGER_addToDB(void)
   add_to_db(db1, product1);
   add_to_db(db1, product2);
 
-  CU_ASSERT(equalProducts(db1->product[0], product1));
-  CU_ASSERT(equalProducts(db1->product[1], product2));
+  CU_ASSERT(equal_products(db1->product[0], product1));
+  CU_ASSERT(equal_products(db1->product[1], product2));
 }
 /**
-@brief Tests the funcion equalDatabases
+@brief Tests the funcion equal_databases
  */
-void testLAGER_equalDatabases(void)
+void testLAGER_equal_databases(void)
 {
   db_t db1 = malloc(sizeof(struct db_t));
   db_t db2 = malloc(sizeof(struct db_t));
@@ -92,12 +92,12 @@ void testLAGER_equalDatabases(void)
   add_to_db(db1, product1);
   add_to_db(db2, product1);
 
-  CU_ASSERT(equalDatabases(db1, db2) == true);
+  CU_ASSERT(equal_databases(db1, db2) == true);
 }
 /**
-@brief Tests the function positionOfProduct
+@brief Tests the function position_of_product
  */
-void testLAGER_positionOfProduct(void)
+void testLAGER_position_of_product(void)
 {
   db_t db1 = malloc(sizeof(struct db_t));
   struct product_t product1;
@@ -117,13 +117,13 @@ void testLAGER_positionOfProduct(void)
   add_to_db(db1, product1);
   add_to_db(db1, product2);
   
-  CU_ASSERT(positionOfProduct(db1, product1.name) == 0);
-  CU_ASSERT(positionOfProduct(db1, product2.name) == 1);
+  CU_ASSERT(position_of_product(db1, product1.name) == 0);
+  CU_ASSERT(position_of_product(db1, product2.name) == 1);
 }
 /**
-@brief Tests the function placeExist
+@brief Tests the function place_exist
  */
-void testLAGER_placeExist(void)
+void testLAGER_place_exist(void)
 {
   db_t db1 = malloc(sizeof(struct db_t));
   struct product_t product1;
@@ -143,16 +143,16 @@ void testLAGER_placeExist(void)
   add_to_db(db1, product1);
   add_to_db(db1, product2);
   
-  CU_ASSERT(placeExist(db1, product1.place) == 0);
-  CU_ASSERT(placeExist(db1, product2.place) == 1);
+  CU_ASSERT(place_exist(db1, product1.place) == 0);
+  CU_ASSERT(place_exist(db1, product2.place) == 1);
 }
 
 /**
-@brief Tests the function dbCopy and looks if it is equal to the database it copies.
+@brief Tests the function db_copy and looks if it is equal to the database it copies.
 */
-void testLAGER_dbCopy(void) {
+void testLAGER_db_copy(void) {
   db_t db1 = malloc(sizeof(struct db_t));
-  CU_ASSERT(equalDatabases(db_copy(db1),db1));
+  CU_ASSERT(equal_databases(db_copy(db1),db1));
 }
 
 /**
@@ -161,10 +161,10 @@ void testLAGER_dbCopy(void) {
 void testLAGER_scenario(void) {
   db_t db1 = malloc(sizeof(struct db_t));
   add_item(db1);
-  CU_ASSERT(positionOfProduct(db1, "Apple\n") == 0);
-  CU_ASSERT(placeExist(db1, "A01\n") == 0);
+  CU_ASSERT(position_of_product(db1, "Apple\n") == 0);
+  CU_ASSERT(place_exist(db1, "A01\n") == 0);
   edit_item(db1);
-  CU_ASSERT(placeExist(db1, "B3\n") == 0);
+  CU_ASSERT(place_exist(db1, "B3\n") == 0);
   edit_item(db1);
   CU_ASSERT(strcmp(db1->product[0].description, "A round ripe fruit\n") == 0);
   edit_item(db1);
@@ -172,9 +172,9 @@ void testLAGER_scenario(void) {
   edit_item(db1);
   CU_ASSERT(db1->product[0].amount == 5);
   edit_item(db1);
-  CU_ASSERT(positionOfProduct(db1, "Orange\n") == 0);
+  CU_ASSERT(position_of_product(db1, "Orange\n") == 0);
   remove_item(db1);
-  CU_ASSERT(positionOfProduct(db1, "Orange\n") == -1);
+  CU_ASSERT(position_of_product(db1, "Orange\n") == -1);
 }
 
 /**
@@ -199,12 +199,12 @@ int main()
 
   /* add the tests to the suites */
   if (
-      (NULL == CU_add_test(pSuite1, "test of lager_equalProducts()", testLAGER_equalProducts)) ||
-      (NULL == CU_add_test(pSuite1, "test of lager_addToDB()", testLAGER_addToDB)) ||
-      (NULL == CU_add_test(pSuite1, "test of lager_equalDatabases()", testLAGER_equalDatabases)) ||
-      (NULL == CU_add_test(pSuite1, "test of lager_positionOfProduct()", testLAGER_positionOfProduct)) ||
-      (NULL == CU_add_test(pSuite1, "test of lager_placeExist()", testLAGER_placeExist)) ||
-      (NULL == CU_add_test(pSuite1, "test of lager_dbCopy()", testLAGER_dbCopy)) ||
+      (NULL == CU_add_test(pSuite1, "test of lager_equal_products()", testLAGER_equal_products)) ||
+      (NULL == CU_add_test(pSuite1, "test of lager_add_to_db()", testLAGER_add_to_db)) ||
+      (NULL == CU_add_test(pSuite1, "test of lager_equal_databases()", testLAGER_equal_databases)) ||
+      (NULL == CU_add_test(pSuite1, "test of lager_position_of_product()", testLAGER_position_of_product)) ||
+      (NULL == CU_add_test(pSuite1, "test of lager_place_exist()", testLAGER_place_exist)) ||
+      (NULL == CU_add_test(pSuite1, "test of lager_db_copy()", testLAGER_db_copy)) ||
       (NULL == CU_add_test(pSuite1, "test of lager_scenario()", testLAGER_scenario))
       )
     {
