4af8ff24a4824b84feccad7a305e4e09762a773b
Rebecca Oskarsson <Rebecca@student94-208.eduroam.uu.se>
2014-12-10 13:57:56 +0100

    Doxy correct

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['add_5fitem',['add_item',['../lagerhantering_8c.html#acc9ceeb7566ae77ac7099b421642e8c2',1,'lagerhantering.c']]],
-  ['add_5fto_5fdb',['add_to_db',['../lagerhantering_8c.html#a76cd68e07c24b10417bec51f75ade554',1,'lagerhantering.c']]],
-  ['ask_5fchar_5fquestion',['ask_char_question',['../lagerhantering_8c.html#a4841b056105d63f2f516ab058c8b4bc6',1,'lagerhantering.c']]],
-  ['ask_5fint_5fquestion',['ask_int_question',['../lagerhantering_8c.html#a3e97e9089a34875614eaf8dca20a1646',1,'lagerhantering.c']]],
-  ['ask_5fstring_5fquestion',['ask_string_question',['../lagerhantering_8c.html#a70130c4594a54dc7bc118945bdef9663',1,'lagerhantering.c']]]
+  ['add_5fitem',['add_item',['../lagerhantering_8h.html#acc9ceeb7566ae77ac7099b421642e8c2',1,'add_item(db_t db):&#160;lagerhantering.c'],['../lagerhantering_8c.html#acc9ceeb7566ae77ac7099b421642e8c2',1,'add_item(db_t db):&#160;lagerhantering.c']]],
+  ['add_5fto_5fdb',['add_to_db',['../lagerhantering_8h.html#a76cd68e07c24b10417bec51f75ade554',1,'add_to_db(db_t db, struct product_t product):&#160;lagerhantering.c'],['../lagerhantering_8c.html#a76cd68e07c24b10417bec51f75ade554',1,'add_to_db(db_t db, struct product_t product):&#160;lagerhantering.c']]],
+  ['ask_5fchar_5fquestion',['ask_char_question',['../lagerhantering_8h.html#a4841b056105d63f2f516ab058c8b4bc6',1,'ask_char_question(char *question, char *alternative):&#160;lagerhantering.c'],['../lagerhantering_8c.html#a4841b056105d63f2f516ab058c8b4bc6',1,'ask_char_question(char *question, char *alternative):&#160;lagerhantering.c']]],
+  ['ask_5fint_5fquestion',['ask_int_question',['../lagerhantering_8h.html#a3e97e9089a34875614eaf8dca20a1646',1,'ask_int_question(char *question):&#160;lagerhantering.c'],['../lagerhantering_8c.html#a3e97e9089a34875614eaf8dca20a1646',1,'ask_int_question(char *question):&#160;lagerhantering.c']]],
+  ['ask_5fstring_5fquestion',['ask_string_question',['../lagerhantering_8h.html#a70130c4594a54dc7bc118945bdef9663',1,'ask_string_question(char *question):&#160;lagerhantering.c'],['../lagerhantering_8c.html#a70130c4594a54dc7bc118945bdef9663',1,'ask_string_question(char *question):&#160;lagerhantering.c']]]
 ];
